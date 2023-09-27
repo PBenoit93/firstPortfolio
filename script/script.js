7b1b5e1e4fc5202aed1f06ec24817ca448a57733
PBenoit93 <pierre.benoit93@gmail.com>
2023-09-27 18:44:42 +0200

    fct nav

--- script/script.js
+++ script/script.js
@@ -1,1 +1,30 @@
 console.log('test')
+
+
+function nav(visible){
+    
+    let pages = document.querySelectorAll(".page")
+    pages.forEach(page => {
+
+        console.log(page.id)
+        console.log(visible)
+
+        if(page.id===visible){
+            if(page.classList.contains("display-none")){
+                page.classList.remove("display-none")
+            }
+            if(!(page.classList.contains("display-flex"))){
+                page.classList.add("display-flex")
+            }
+        }else{
+            if(page.classList.contains("display-flex")){
+                page.classList.remove("display-flex")
+            }
+            if(!(page.classList.contains("display-none"))){
+                page.classList.add("display-none")
+            }
+        }
+
+    })
+
+}
