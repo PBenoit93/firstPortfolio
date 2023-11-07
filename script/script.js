b69cb1357f77ac43436d155d6268dbed71429d82
PBenoit93 <pierre.benoit93@gmail.com>
2023-11-07 02:28:03 +0100

    css V2

--- script/script.js
+++ script/script.js
@@ -6,12 +6,9 @@ function nav(visible){
     let pages = document.querySelectorAll(".page")
     pages.forEach(page => {
 
-        console.log(page.id)
-        console.log(visible)
-
         if(page.id===visible){
-            if(page.classList.contains("display-none")){
-                page.classList.remove("display-none")
+            if(page.classList.contains("displayNone")){
+                page.classList.remove("displayNone")
             }
             if(!(page.classList.contains("display-flex"))){
                 page.classList.add("display-flex")
@@ -20,8 +17,8 @@ function nav(visible){
             if(page.classList.contains("display-flex")){
                 page.classList.remove("display-flex")
             }
-            if(!(page.classList.contains("display-none"))){
-                page.classList.add("display-none")
+            if(!(page.classList.contains("displayNone"))){
+                page.classList.add("displayNone")
             }
         }
 
